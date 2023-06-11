60ecdb3ada34aa3c7ba798fa4b6e6ac9c28e6640
pelayo93 <jesuspelayo93@gmail.com>
2023-06-11 18:37:05 -0400

    Tu mensaje de commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+function Error ({ msg }) {
+  return (
+    <>
+      <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded'><p>{msg}</p></div>
+    </>
+  )
+}
+
+export default Error
